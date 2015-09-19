0506c1f030d09f359aae3d2ab4e28d67bbe2e8bf
David Froger <david.froger@inria.fr>
2015-09-19 12:35:57 +0200

    MyQueue picked values are correct

--- myqueue/myqueue.py
+++ myqueue/myqueue.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 
 from .mystack import MyStack
+from .error import Empty
 
 class MyQueue:
     def __init__(self):
         self._size = 0
         self._incoming = MyStack()
+        self._outgoing = MyStack()
 
     @property
     def size(self):
         return self._size
 
     def add(self, item):
+        self._incoming.add(item)
         self._size += 1
 
     def peek(self):
+        if not self._outgoing.size:
+            if self._incoming.size:
+                self._decant_incoming_into_outgoing()
+            else:
+                raise Empty("Can't peek item from empty queue")
+        item = self._outgoing.peek()
         self._size -= 1
+        return item
+
+    def _decant_incoming_into_outgoing(self):
+        while self._incoming.size:
+            item = self._incoming.peek()
+            self._outgoing.add(item)
--- test/test_myqueue.py
+++ test/test_myqueue.py
@@ -8,19 +8,50 @@ class TestMyQueue(unittest.TestCase):
         queue = myqueue.MyQueue()
         self.assertEqual(queue.size, 0)
 
-    def test_add(self):
+    def test_add_and_peek_one_item(self):
         # Create an empty queue.
         queue = myqueue.MyQueue()
         self.assertEqual(queue.size, 0)
 
-        # Add a item in the queue.
+        # Add item 42
         queue.add(42)
         self.assertEqual(queue.size, 1)
 
-        # Peek a item from the queue.
+        # Peek item item 42
         item = queue.peek()
         self.assertEqual(queue.size, 0)
-        #self.assertEqual(item, 42)
+        self.assertEqual(item, 42)
+
+    def test_add_and_peek_two_items(self):
+        # Create an empty queue.
+        queue = myqueue.MyQueue()
+        self.assertEqual(queue.size, 0)
+
+        # Add item 42
+        queue.add(42)
+        self.assertEqual(queue.size, 1)
+
+        # Add item 43
+        queue.add(43)
+        self.assertEqual(queue.size, 2)
+
+        # Peek item item 42
+        item = queue.peek()
+        self.assertEqual(queue.size, 1)
+        self.assertEqual(item, 42)
+
+        # Peek item item 43
+        item = queue.peek()
+        self.assertEqual(queue.size, 0)
+        self.assertEqual(item, 43)
+
+    def test_peek_empty_queue(self):
+        # Create empty queue
+        queue = myqueue.MyQueue()
+
+        msg = "Can't peek item from empty queue"
+        with self.assertRaisesRegexp(myqueue.Empty, msg):
+            queue.peek()
 
 if __name__ == '__main__':
     unittest.main()
